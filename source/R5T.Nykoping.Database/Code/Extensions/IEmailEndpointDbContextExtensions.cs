f81c7eb70c725d185d50b5c6480fb2a17332ae43
MinexAutomation <minex.automation@gmail.com>
2020-01-05 14:41:07 -0800

    Added IEmailEndpointDbContext extensions.

---
+++ source/R5T.Nykoping.Database/Code/Extensions/IEmailEndpointDbContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+using R5T.Endalia;
+
+using EmailEndpointEntity = R5T.Nykoping.Database.Entities.EmailEndpoint;
+
+
+namespace R5T.Nykoping.Database
+{
+    public static class IEmailEndpointDbContextExtensions
+    {
+        public static IQueryable<EmailEndpointEntity> GetEmailEndpoint(this IEmailEndpointDbContext dbContext, EndpointIdentity endpointIdentity)
+        {
+            var emailEndpointQueryable = dbContext.EmailEndpoints.Where(x => x.EndpointGUID == endpointIdentity.Value);
+            return emailEndpointQueryable;
+        }
+    }
+}
